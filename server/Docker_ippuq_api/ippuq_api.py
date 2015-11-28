38c80fc7628cae7b4d622dc67fd9ec12fb71d5c7
ITO Shunichi <ito@heartbeats.jp>
2015-11-29 08:26:10 +0900

    fixed #18 and fixed #17

--- server/Docker_ippuq_api/ippuq_api.py
+++ server/Docker_ippuq_api/ippuq_api.py
@@ -7,9 +7,11 @@ from flask import Flask, jsonify, request, render_template
 from flask.ext.api import status
 from flask_restful import Resource, Api, reqparse
 from pyapns import configure, provision, notify
+from datetime import datetime
 import json
 import requests
 import pymysql
+import time
 app = Flask(__name__)
 api = Api(app)
 
@@ -36,7 +38,7 @@ class users(Resource):
         400: bad request
         '''
         cur = db.cursor()
-        content_body_dict = json.loads(request.data)
+        content_body_dict = request.get_json()
         CHECK_EXIST_USER_SQL = 'SELECT user_name FROM users WHERE user_name = %s AND passwd = %s'
         CREATE_USER_SQL = 'INSERT INTO users VALUE(%s, %s, %s, %s)'
         _check_exist_user = cur.execute(CHECK_EXIST_USER_SQL,
@@ -61,6 +63,27 @@ class users(Resource):
                 return "", status.HTTP_201_CREATED
             except pymysql.OperationalError:
                 return "", status.HTTP_500_INTERNAL_SERVER_ERROR
+    def get(self):
+        '''
+        [json]
+        {user_name:xxx, passwd:xxx}
+        [return code]
+        200: OK
+        400: NG
+        '''
+
+        cur = db.cursor()
+        content_body_dict = request.get_json()
+        CHECK_LOGIN_USER_SQL = 'SELECT user_name FROM users WHERE user_name = %s AND passwd = %s'
+        _check_correct_user = cur.execute(CHECK_LOGIN_USER_SQL, (content_body_dict['user_name', content_body_dict['passwd']]))
+        if _check_correct_user is 1:
+            response = {'message':'login success'}
+            return response, status.HTTP_200_OK
+        else:
+            response = {'message': 'login failed'}
+            return response, status.HTTP_400_BAD_REQUEST
+
+class places(Resource):
 
     def get(self):
         '''
@@ -79,5 +102,31 @@ class users(Resource):
         _count_dict = cur.fetchall()
         return render_template("test.html", counts=_count_dict), status.HTTP_200_OK
 
+    def put(self):
+        cur = db.cursor()
+        content_body_dict = request.get_json()
+        SEARCH_USER_ID_SQL = 'SELECT user_id FROM users WHERE user_name = %s'
+        cur.execute(SEARCH_USER_ID_SQL, (content_body_dict['user_name'],))
+        _user_id = cur.fetchone()[0]
+        print _user_id
+        print  content_body_dict['len']
+        print  content_body_dict['lng']
+
+        INSERT_PLACE_SQL = 'INSERT INTO place(user_id, used_beacon_id, lat, lng) VALUES(%s, %s, %s, %s )'
+        try:
+            test = cur.execute(INSERT_PLACE_SQL,
+                        (_user_id,
+                         content_body_dict['beacon_id'],
+                         str(content_body_dict['len']),
+                         str(content_body_dict['lng'])
+                        )
+                       )
+            db.commit()
+            return '', status.HTTP_200_OK
+        except pymysql.OperationalError:
+            return '', status.HTTP_500_INTERNAL_SERVER_ERROR
+
+
 api.add_resource(users, '/users')
+api.add_resource(places, '/places')
 app.run(host='0.0.0.0', port=80, debug=True)
