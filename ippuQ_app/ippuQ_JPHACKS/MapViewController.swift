4d255982307d6c742c2f04afe3187e40a9da9da7
yakipote <yakipote@redimpulz.com>
2015-11-29 05:34:33 +0900

    #14 mapview added

--- ippuQ_app/ippuQ_JPHACKS/MapViewController.swift
+++ ippuQ_app/ippuQ_JPHACKS/MapViewController.swift
@@ -9,34 +9,41 @@
 
 import Foundation
 import UIKit
+import MapKit
+import Darwin
 
-class MapViewController :UIViewController{
-    var delegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-    
-    @IBOutlet weak var MapWebView: UIWebView!
-    
-    var targetURL:String?
-    
-    
-    
+
+class MapViewController :UIViewController,MKMapViewDelegate{
+    @IBOutlet var mapView:MKMapView!
+    var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //AppDelegateのインスタンスを取得
     override func viewDidLoad() {
         super.viewDidLoad()
-        targetURL = "http://210.140.162.64:10080/view/map?l=\(self.delegate.lat!)&l=\(self.delegate.lng!)"
-        print(targetURL)
         
-        loadAddressURL()
+        // 緯度・軽度を設定
+        let location:CLLocationCoordinate2D
+        = CLLocationCoordinate2DMake(atof(appDelegate.lat!),atof(appDelegate.lng!))
+        
+        mapView.setCenterCoordinate(location,animated:true)
+        
+        // 縮尺を設定
+        var region:MKCoordinateRegion = mapView.region
+        region.center = location
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        
+        mapView.setRegion(region,animated:true)
+        
+        // 表示タイプを航空写真と地図のハイブリッドに設定
+              mapView.mapType = MKMapType.Standard
+        //        mapView.mapType = MKMapType.Satellite
+        //mapView.mapType = MKMapType.Hybrid
         
     }
     
-    
-    func loadAddressURL() {
-        let requestURL = NSURL(string: targetURL!)
-        let req = NSURLRequest(URL: requestURL!)
-        MapWebView.loadRequest(req)
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
-    
-    
     
     
 }
